6699fd99c6d9ebff2c8a944175561412f376d0c3
andrewosh <andrewosh@gmail.com>
2015-05-24 15:22:00 -0400

    Added the Copier utility and changes some inheritance stuff

---
+++ python/ts2/util/copier.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from threading import Thread
+
+class Copier(object):
+    """
+    The Copier copies all ordered files in one directory tree into another directory, with an optional
+    delay after each copy.
+    """
+
+    def __init__(self, dirs):
+        Thread.__init__(self)
+        self.dirs = dirs
+
+    def copy_dir(self, src, dst):
+        fs = []
+        for (root, dir, file) in os.walk(src):
+            fs.append(os.path.join(root, file))
+        for f in sorted(fs):
+            shutil.copy(f, dst)
+
+    def start(self):
+        self.threads = []
+        for src, dst in self.dirs:
+            t = Thread(self.copy_dir, args=(src, dst))
+            self.threads.append(t)
+            t.start()
+
+    def stop(self):
+        for thread in self.threads:
+            thread.stop()
