ad668d326dd202714b9d3e822d627e0b7fc9ff2a
andrewosh <andrewosh@gmail.com>
2015-06-19 16:02:06 -0400

    Changed table configurtion params

--- scala/src/main/scala/thunder_streaming/receivers/HBaseReceiver.scala
+++ scala/src/main/scala/thunder_streaming/receivers/HBaseReceiver.scala
@@ -2,12 +2,15 @@ package thunder_streaming.receivers
 
 import java.util
 
+import org.apache.hadoop.fs.Path
 import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.receiver.Receiver
 import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.{HBaseAdmin, Scan, HTable}
+import org.apache.hadoop.hbase.client.Scan
+import org.apache.hadoop.hbase.client.Connection
+import org.apache.hadoop.hbase.client.ConnectionFactory
 import org.apache.hadoop.hbase.filter.{SingleColumnValueFilter, FilterList}
 import scala.util.control.Breaks._
 
@@ -43,47 +46,58 @@ class HBaseReceiver(reqCols: util.ArrayList[String],
    * Apply batchScan to 'data' table in HBase and
    */
   def receive(): Unit = {
+
     val conf = HBaseConfiguration.create()
-    val admin = new HBaseAdmin(conf)
-    val table = new HTable(conf, DATA_TABLE)
+    conf.addResource(new Path(System.getenv("HBASE_CONF_DIR"), "hbase-site.xml"))
+    conf.addResource(new Path(System.getenv("HADOOP_CONF_DIR"), "core-site.xml"))
 
-    var filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL)
-    val filters = reqCols.toList.map{ col =>
-      // If the column exists (it's not equal to the null terminator byte), return true
-      val filter = new SingleColumnValueFilter(Bytes.toBytes(family), Bytes.toBytes(col), CompareOp.NOT_EQUAL, Bytes.toBytes('\0'))
-      filter.setFilterIfMissing(true)
-      filter
-    }
-    filters.foreach(f => filterList.addFilter(f))
-
-    var batchScan = new Scan().setFilter(filterList)
-
-    // minRow is updated after each batch to reflect the last complete row successfully stored
-    var minRow = 0
-
-    while (!stopped) {
-      val startRow = getPaddedKey(minRow.toString)
-      println("startRow: %s".format(startRow))
-      batchScan.setStartRow(Bytes.toBytes(startRow))
-      val resultScanner = table.getScanner(batchScan)
-      val res = resultScanner.next()
-      while(res != null) {
-        val row = Bytes.toString(res.getRow())
-        println("Got row: %s".format(row))
-        val rowVal = row.toInt
-        if (rowVal - minRow > 1) {
-          // If the difference between the last valid row and this row is >1, then stop the current
-          // iteration
-          break
+    val conn = ConnectionFactory.createConnection(conf)
+    try {
+      val table = conn.getTable(TableName.valueOf(DATA_TABLE))
+      try {
+        var filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL)
+        val filters = reqCols.toList.map { col =>
+          // If the column exists (it's not equal to the null terminator byte), return true
+          val filter = new SingleColumnValueFilter(Bytes.toBytes(family), Bytes.toBytes(col), CompareOp.NOT_EQUAL, Bytes.toBytes('\0'))
+          filter.setFilterIfMissing(true)
+          filter
         }
-        if (rowVal > minRow) {
-          minRow = rowVal
+        filters.foreach(f => filterList.addFilter(f))
+
+        var batchScan = new Scan().setFilter(filterList)
+
+        // minRow is updated after each batch to reflect the last complete row successfully stored
+        var minRow = 0
+
+        while (!stopped) {
+          val startRow = getPaddedKey(minRow.toString)
+          println("startRow: %s".format(startRow))
+          batchScan.setStartRow(Bytes.toBytes(startRow))
+          val resultScanner = table.getScanner(batchScan)
+          val res = resultScanner.next()
+          while (res != null) {
+            val row = Bytes.toString(res.getRow())
+            println("Got row: %s".format(row))
+            val rowVal = row.toInt
+            if (rowVal - minRow > 1) {
+              // If the difference between the last valid row and this row is >1, then stop the current
+              // iteration
+              break
+            }
+            if (rowVal > minRow) {
+              minRow = rowVal
+            }
+            val cols = res.getValue(Bytes.toBytes(family), Bytes.toBytes(dataSet))
+            println("Storing row of length: %d".format(cols.length))
+            store((row, cols))
+          }
+          Thread.sleep(period * 1000)
         }
-        val cols = res.getValue(Bytes.toBytes(family), Bytes.toBytes(dataSet))
-        println("Storing row of length: %d".format(cols.length))
-        store((row, cols))
+      } finally {
+        table.close()
       }
-      Thread.sleep(period * 1000)
+    } finally {
+      conn.close()
     }
   }
 
